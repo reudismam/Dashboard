aba225d14705958137e8edb63d20c6fd69c87506
reudismam <reudismam@gmail.com>
2025-11-27 20:54:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-afdsf
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df=pd.read_csv('/content/vacinacao_corrigido.csv')
+
+fig2=px.line(df,x='date', y='total_vaccinations', color='location',
+             title='Total de pessoas vacinadas por data e país - Segundo OMS')
+
+fig2.update_layout(xaxis_title='Data', yaxis_title='Total de pessoas vacinadas')
+fig2.show()
+
+st.plotly_chart(fig1, use_container_width=True)
